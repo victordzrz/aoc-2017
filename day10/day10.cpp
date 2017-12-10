ae210a381d24ca25b71679a4a4c6ade7d36a1747
victordzrz <victordzrz@gmail.com>
2017-12-10 12:27:50 +0100

    Part 2 working

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -4,40 +4,56 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include  <iomanip>
 
 using namespace std;
 
 
-void reverse(vector<int> &input,int positionStart, int lenght);
-int knotHash(const vector<int> &lenghtSequence,int ringLength);
+void reverse(vector<char> &input,int positionStart, int lenght);
+vector<char> knotHash(const vector<char> &lenghtSequence,int ringLength);
 
-int knotHash(const vector<int> &lenghtSequence,int ringLength){
-  vector<int> numberList;
+vector<char> knotHash(const vector<char> &lenghtSequence,int ringLength,int nRounds){
+  cout << "Starting hash \n"<<flush;
+  vector<char> numberList;
   int skipSize=0;
   int currentPosition=0;
   for(int i=0;i<ringLength;i++){
     numberList.push_back(i);
   }
 
-  for(int length:lenghtSequence){
-    reverse(numberList,currentPosition,length);
+  for(int i=0;i<nRounds;i++){
+    //cout << "Round " << i << "\n" << flush;
+    for(char length:lenghtSequence){
+      reverse(numberList,currentPosition,length);
 
-    cout << "Result: ";
-    for(auto i:numberList){
-      cout << i << " ";
+      //cout << "Result: ";
+      // for(auto i:numberList){
+      //   cout << i << " ";
+      // }
+      //cout << "\n______\n";
+      currentPosition=(currentPosition+length+skipSize)%ringLength;
+      skipSize++;
     }
-    cout << "\n______\n";
-    currentPosition=(currentPosition+length+skipSize)%ringLength;
-    skipSize++;
   }
 
-  return numberList[0]*numberList[1];
+  vector<char> sparse_hash=numberList;
+  vector<char> denseHash;
+  for(int j=0;j<16;j++){
+    char currentHash=0;
+    for(int i=0;i<16;i++){
+      currentHash^=sparse_hash[i+16*j];
+    }
+    denseHash.push_back(currentHash);
+    //cout << ((int)j) << ": " << currentHash << "\n";
+  }
+
+  return denseHash;
 }
 
 
-void reverse(vector<int> &input,int positionStart, int length){
-  vector<int>::iterator first=input.begin()+positionStart;
-  vector<int> copy;
+void reverse(vector<char> &input,int positionStart, int length){
+  vector<char>::iterator first=input.begin()+positionStart;
+  vector<char> copy;
   auto current_it=first;
   for(auto i=0;i<length;i++){
     copy.push_back(*current_it);
@@ -50,7 +66,7 @@ void reverse(vector<int> &input,int positionStart, int length){
   auto copy_it=copy.end()-1;
 
   for(auto i=0;i<length;i++){
-    cout << "switching " << *first << " for " << *copy_it << "\n";
+    //cout << "switching " << *first << " for " << *copy_it << "\n";
     *first=*copy_it;
     copy_it--;
     first++;
@@ -68,27 +84,36 @@ int main(int argc,char* argv[]){
     string inputString;
     getline(inputFile,inputString);
     cout << "Input:\n";
-    for(auto it=inputString.begin();it!=inputString.end();it++){
+    /*for(auto it=inputString.begin();it!=inputString.end();it++){
       if(*it==','){
         *it=' ';
       }
     }
+    stringstream inputStream(inputString);
+
+    */
 
     cout << inputString <<"\n" ;
 
-    stringstream inputStream(inputString);
-    int num;
-    vector<int> numberList;
-    while(inputStream >> num){
-      numberList.push_back(num);
+    vector<char> numberList;
+    for(char c : inputString){
+      numberList.push_back(c);
     }
 
-    for(int i:numberList){
-      cout << i << "\n";
+    vector<char> extra_seq{17,31,73,47,23};
+
+    numberList.insert(numberList.end(),extra_seq.begin(),extra_seq.end());
+
+
+    // for(char i:numberList){
+    //   cout << hex << i << "\n";
+    // }
+
+    vector<char> res=knotHash(numberList,256,64);
+
+    for(int c:res){
+      cout << hex << std::setw(2) << std::setfill('0') << (c&0x000000ff);
     }
-
-    int res=knotHash(numberList,256);
-
-    cout << "Result is: " << res << "\n";
+    cout <<  "\n";
   }
 }
